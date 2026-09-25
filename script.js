c512275a2c5cc7d1478954c2974e3fc8cc8c0c5b
BrianMbithi1 <swael8867@gmail.com>
2026-09-25 22:36:19 +0300

    Declared skills array in js code and added a loop to loop through the array

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 const skills = ["HTML", "CSS", "Javascript", "Python"];
+ const skillsList = document.getElementById("skills-list");
 
-
+ skills.forEach(skill) => {
+    const li = DocumentFragment.createElement("li");
+    li.textContent = skill;
+    skillsList .appendChild
+ }
